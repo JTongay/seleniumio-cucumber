cae4864c74985cc4962e63422298df8c8cbbf936
Joseph Tongay <dirk.tongay@gmail.com>
2017-02-04 14:46:35 -0600

    test is doing the thing on the initial get to google.com

--- features/google_search.feature
+++ features/google_search.feature
@@ -1,4 +1,13 @@
-Feature: title
-  In order to value
-  As a role
-  I want feature
+Feature: Do a Google Search
+  Using a web browser
+  I want to perform a Google search
+
+  Scenario: Google Search
+    Given I visit http://google.com
+    When I enter 'cheese' into 'gbqfq'
+    Then I should see search results
+
+  Scenario: Google Search
+    Given I visit http://google.com
+    When I enter 'hamburgers' into 'gbqfq'
+    Then I should see search results
---
+++ features/step_definitions/google_search.js
@@ -0,0 +1,68 @@
+// module.exports = function () {
+//     console.log('Do a Google Search Steps Loaded');
+//     this.World = require('../support/world');
+//
+//     /* "<Then> I should see search results" */
+//     this.Then(/^I should see search results$/, function (callback) {
+//
+//         var inputId = '#resultStats';
+//
+//         this.waitFor(inputId, 3000, function (err) {
+//
+//             if (err) {
+//                 return callback.fail(new Error('Element ' + inputId + ' was not found after 3s'));
+//             }
+//
+//             this.getText(inputId, function (err, text) {
+//
+//                 text = (''+text).match(/^About ([\d,]+) .*$/);
+//                 text = parseInt(text[1].replace(/,/g,''), 10);
+//
+//                 // Fail this test if the search results are null
+//                 if (text === 0 || isNaN(text))
+//                     return callback.fail(new Error('Expected to see some results, but saw 0 (captured result count is ' + number + ')'));
+//
+//                 // Pass and say about it.
+//                 console.log( '(Saw ' + text + ' results)');
+//                 return callback();
+//             });
+//
+//         });
+//     });
+// };
+// features/step_definitions/browser_steps.js
+require('chromedriver')
+var {defineSupportCode} = require('cucumber');
+var webdriverio = require('webdriverio');
+var options = { desiredCapabilities: { browserName: 'chrome' } };
+var client = webdriverio.remote(options);
+
+defineSupportCode(function({setDefaultTimeout}) {
+  setDefaultTimeout(60 * 1000);
+});
+
+defineSupportCode(function({Before, Given, When, Then}) {
+
+  Before({timeout: 60 * 1000}, function() {
+   // Does some slow browser/filesystem/network actions
+  });
+
+  Given('I visit http://google.com', function() {
+    client
+      .init()
+      .url('https://duckduckgo.com/')
+      .end()
+  });
+
+  When('I enter {stringInSingleQuotes} into {stringInSingleQuotes}', function (text) {
+    this.driver.findElement({linkText: text}).then(function(element) {
+      return element.click();
+    });
+  });
+
+  Then('I should see search results', function (text) {
+    var xpath = "//*[contains(text(),'" + text + "')]";
+    var condition = seleniumWebdriver.until.elementLocated({xpath: xpath});
+    this.driver.wait(condition, 5000);
+  });
+});
